4582965282ba6b3fbcc8c8230de2114deb607cf6
GoldBeardSea <timbusch1@gmail.com>
2022-11-14 23:46:56 -0800

    normalized address field by iterating and removing comma before APT

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
             String line = "";
             // read the csv
             while ((line = fileReader.readLine()) != null) {
-                String[] splitStr = line.replaceAll("\"", "").split(",");
+                String[] splitStr = line.replaceAll("\"", "").replaceAll("\\.", "").split(",");
                 //get rid of double quotes and split the line on commas
                 StringBuilder sb = new StringBuilder(splitStr[2].replaceAll("\\.", "").replaceAll(",", "").toUpperCase().trim()+ ",");
                 //create a stringbuilder with the first address field to build a standardized address field since inputs are not standardized
@@ -25,11 +25,18 @@ public class Main {
                         sb.append(" " + splitStr[i].replaceAll("\\.", "").toUpperCase().trim() + ",");
                     }
                 }
-                //create AddressEntry objects
-//                System.out.println(sb.toString());
-//                System.out.println(Arrays.toString(splitStr));
+                //create AddressEntry objects standardize apartment with no comma this is essentially a hack, and would need to be further developed to handle additional usecases and alternative spellings
+                if (sb.toString().contains(", APT")) {
+                    System.out.println(true);
+                    for (int i = 0; i < sb.length(); i++) {
+                        if (sb.charAt(i) == ',') {
+                            System.out.println("Delete");
+                            sb.deleteCharAt(i);
+                            break;
+                        }
+                    }
+                }
                 AddressEntry addressEntry = new AddressEntry(splitStr[0], splitStr[1], sb.toString(), splitStr[splitStr.length-1]);
-//                System.out.println(addressEntry.toString());
                 addressBook.addEntry(addressEntry);
             }
             addressBook.simpleToString();
